b7080852ecc78296921439b456ed294e4a9ce953
JuacoDev <Juaco@tellez.pro>
2024-09-28 11:00:47 -0500

    Método de múltiplicación

--- Calculadora.java
+++ Calculadora.java
@@ -4,6 +4,12 @@ public class Calculadora {
         return a + b;
     }
 
+    public static int multiplicar(int num1, int num2){
+        
+        return num1 * num2;
+
+    }
+
 
     public static void main(String[] args) {
         int num1 = 10;
@@ -12,6 +18,12 @@ public class Calculadora {
         // Sumar
         int resultadoSuma = sumar(num1, num2);
         System.out.println("La suma es: " + resultadoSuma);
+
+        // Restar
+
+        // Multiplicar
+        System.out.println("El producto de los números es: " + multiplicar(num1,num2));
+
     }
 }
 
